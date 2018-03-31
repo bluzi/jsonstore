8509584c59dabbfc5012fe8ecb4387fb08dc232e
Eliran Pe'er <eliran013@gmail.com>
2018-03-31 18:07:43 +0300

    get-link: get url dynamically by referer

--- api/routes.js
+++ api/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const crypto = require('crypto');
 const database = require('./database');
+const { URL } = require('url');
 
 const router = express.Router();
 
@@ -8,7 +9,8 @@ router.get('/get-link', (req, res) => {
     const seed = Math.random().toString();
     const hash = crypto.createHash('sha256').update(seed).digest('hex');
 
-    return res.send({ link: `https://jsonstore.io/${hash}/` });
+    const url = new URL(req.get('referer'));
+    return res.send({ link: `${url.protocol}//${url.hostname}/${hash}/` });
 });
 
 router.get(/^\/[0-9a-z]{64}/, (req, res) =>
