024a87541bc8615e9a7600dc1d443a3971e4b9f5
Eliran Pe'er <eliran013@gmail.com>
2018-03-31 16:17:10 +0300

    allow running without env var

--- api/database.js
+++ api/database.js
@@ -1,31 +1,40 @@
 const firebase = require('firebase');
 
-const config = JSON.parse(process.env.FIREBASE_CONFIG);
-firebase.initializeApp(config);
+if (!process.env.FIREBASE_CONFIG) {
+    module.exports = { 
+        get: console.log,
+        post: console.log,
+        put: console.log,
+        delete: console.log,
+    }
+} else {
+    const config = JSON.parse(process.env.FIREBASE_CONFIG);
+    firebase.initializeApp(config);
 
-module.exports = {
-    get: key =>
-        firebase
-            .database()
-            .ref(key)
-            .once('value')
-            .then(snapshot => snapshot.val()),
+    module.exports = {
+        get: key =>
+            firebase
+                .database()
+                .ref(key)
+                .once('value')
+                .then(snapshot => snapshot.val()),
 
-    post: (key, data) =>
-        firebase
-            .database()
-            .ref(key)
-            .set(data),
+        post: (key, data) =>
+            firebase
+                .database()
+                .ref(key)
+                .set(data),
 
-    put: (key, data) =>
-        firebase
-            .database()
-            .ref(key)
-            .update(data),
+        put: (key, data) =>
+            firebase
+                .database()
+                .ref(key)
+                .update(data),
 
-    delete: key =>
-        firebase
-            .database()
-            .ref(key)
-            .set(undefined),
+        delete: key =>
+            firebase
+                .database()
+                .ref(key)
+                .set(undefined),
+    }
 }
